1aa5cf55c2650e7b2e70b2dfe3440740130bae3e
shentao <shentao@sailvan.com>
2016-09-02 17:42:30 +0800

    revise project

--- Apps/Common/Conf/config.php
+++ Apps/Common/Conf/config.php
@@ -9,7 +9,7 @@ return array(
 	//数据库配置
 	'DB_TYPE' => 'mysql',     // 数据库类型
     'DB_HOST' => '127.0.0.1', // 服务器地址
-    'DB_NAME' => 'test_think',          // 数据库名
+    'DB_NAME' => 'test_amazon',          // 数据库名
     'DB_USER' => 'root',      // 用户名
     'DB_PWD' => 'root',          // 密码
     'DB_PORT' => '3306',        // 端口
@@ -20,5 +20,5 @@ return array(
     'TMPL_L_DELIM' => '<{',            // 模板引擎普通标签开始标记
     'TMPL_R_DELIM' => '}>',            // 模板引擎普通标签结束标记
 
-    'SHOW_PAGE_TRACE' => TRUE,
+    'SHOW_PAGE_TRACE' => true,
 );
--- Public/index.php
+++ Public/index.php
@@ -20,23 +20,5 @@ define('APP_DEBUG',true);
 define('APP_PATH', dirname(__DIR__) . DIRECTORY_SEPARATOR . 'Apps' . DIRECTORY_SEPARATOR);
 define('RUNTIME_PATH', dirname(__FILE__) . DIRECTORY_SEPARATOR . 'Runtime' . DIRECTORY_SEPARATOR);
 
-// Server params
-$scriptName = $_SERVER['SCRIPT_NAME']; // <-- "/foo/index.php"
-$requestUri = $_SERVER['REQUEST_URI']; // <-- "/foo/bar?test=abc" or "/foo/index.php/bar?test=abc"
-// 物理路径
-if (strpos($requestUri, $scriptName) !== false) {
-    // 没有重写
-    $physicalPath = $scriptName;
-} else {
-    // 有重写
-    $physicalPath = str_replace('\\', '', dirname($scriptName));
-}
-$script_name = rtrim($physicalPath, '/');
-
-// 获取并定义路径常量信息
-define("ROOT_PATH", __DIR__ . DIRECTORY_SEPARATOR);
-define("UPLOAD_PATH", ROOT_PATH . 'Uploads' . DIRECTORY_SEPARATOR);
-define("BASE_URL", $script_name . '/');
-define("UPLOAD_URL", $script_name . '/Uploads/');
 // 引入ThinkPHP入口文件
 require '../Framework/ThinkPHP.php';
